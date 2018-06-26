7fa5399c3babdd41c3c3f57682f84079a47626c0
Ashan Perera <ashan.perera@hotelquickly.com>
2018-06-26 21:40:23 +0700

    Init repo

---
+++ server/server.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const path = require('path')
+const express = require('express')
+
+const publicPath = path.join(__dirname, '../public');
+
+const app = express()
+const port = process.env.port || 3000
+app.use(express.static(publicPath))
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
